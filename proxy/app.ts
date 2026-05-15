7519c83b78efaf17bf1414d80302d23342585b10
julienb84 <julienbouchez@icloud.com>
2026-05-15 11:45:09 +0200

    save app.ts proxy

--- proxy/app.ts
+++ proxy/app.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
-import process from 'process';
-
-dotenv.config();
+require("dotenv").config();
 
 // Charge ton app ES module de façon asynchrone
-import('./index.js').catch(err => {
-    console.error('Failed to load app:', err);
-    process.exit(1);
+import("./index.js").catch((err) => {
+  console.error("Failed to load app:", err);
+  process.exit(1);
 });
